a67bdd85d7843dbbf502a222553c77ebf1c634cd
betogroo <luizhumberto@gmail.com>
2025-07-10 15:17:33 -0300

    chore: add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import useTitle from '@/hooks/useTitle'
 import { useEffect } from 'react'
+import { Button } from '@/components/ui/button'
 export default function Home() {
   const { title, setTitle } = useTitle()
 
@@ -8,5 +9,19 @@ export default function Home() {
     setTitle('Home page')
   }, [setTitle])
 
-  return <h1 className="text-2xl">{title}</h1>
+  return (
+    <div
+      className="container flex flex-col items-center
+    "
+    >
+      <h1 className="text-2xl">{title}</h1>
+      <Button
+        className="cursor-pointer"
+        variant={'default'}
+        onClick={() => console.log('Button')}
+      >
+        Button
+      </Button>
+    </div>
+  )
 }
